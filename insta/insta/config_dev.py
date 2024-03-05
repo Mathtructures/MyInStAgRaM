fed8f23b196c4ed1dfe3a12d3ff387063d8ca1c4
Mathtructures <138815355+Mathtructures@users.noreply.github.com>
2024-03-06 00:49:28 +0330

    fixed some bugs and created some new ones!!

--- insta/insta/config_dev.py
+++ insta/insta/config_dev.py
@@ -4,6 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 debug = True
 allowed_hosts = [
     '127.0.0.1',
+    '192.168.1.8',
 ]
 db_engine = 'django.db.backends.sqlite3'
 db_name = BASE_DIR / 'db.sqlite3'
